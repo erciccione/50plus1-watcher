666e85465d83b89d33080da1b4f5705b03b17bf8
erciccione <erciccione@users.noreply.github.com>
2017-10-19 23:10:19 +0200

    add personal class to convert hash

--- 50plus1.rb
+++ 50plus1.rb
@@ -7,6 +7,11 @@ class String
   include Term::ANSIColor
 end
 
+def toHs(hash)
+	hash / 1000000
+end
+
+
 	# get API of all pools
 
 string = open('http://minexmr.com/pools_hist.json')
@@ -28,34 +33,31 @@ miningpoolhub_hr = pools['miningpoolhub'].last
 nanopool_hr = pools['nanopool'].last
 minergate_hr = minergate_api["pool"]["hashrate"] 
 
-
 	# get network's Hashrate
 string = open('http://moneroblocks.info/api/get_stats/')
 read_string = string.read
 network_hr_raw = JSON.parse read_string
 network_hr = network_hr_raw['hashrate']
 	
-
+	
 attck_hr = ((network_hr / 2) * 1.01 )
 pc_hr = 40		# in hs/s
 botnum = (attck_hr / pc_hr)
 
 
-
 puts "Easy tool who calculates the possibility of a 50+1% attack to the Monero network".bold
 puts ""
-puts "Current global Hashrate:" + " #{(network_hr/1000000).round(2)} MH/s".bold
-puts "An attacker should have an hashrate of at least:" + " #{(attck_hr/1000000).round(2)} MH/s".bold
+puts "Current global Hashrate:" + " #{toHs(network_hr).round(2)} MH/s".bold
+puts "An attacker should have an hashrate of at least:" + " #{toHs(attck_hr).round(2)} MH/s".bold
 puts "or a botnet with" + " #{botnum.to_i} bots.".bold + " (calculated assuming 1 bot = 40 Hs/s)"
 
 
-
 	#calculate percentage network
-dwarfpool_perc = ((dwarfpool_hr/(network_hr/1000000))*100).round(2)
-cryptopool_fr_perc = ((cryptopool_fr_hr/(network_hr/1000000))*100).round(2)
-minexmr_perc = ((minexmr_hr/(network_hr/1000000))*100).round(2)
-miningpoolhub_perc = ((miningpoolhub_hr/(network_hr/1000000))*100).round(2)
-nanopool_perc = ((nanopool_hr/(network_hr/1000000))*100).round(2)
+dwarfpool_perc = ((dwarfpool_hr/toHs(network_hr))*100).round(2)
+cryptopool_fr_perc = ((cryptopool_fr_hr/toHs(network_hr))*100).round(2)
+minexmr_perc = ((minexmr_hr/toHs(network_hr))*100).round(2)
+miningpoolhub_perc = ((miningpoolhub_hr/toHs(network_hr))*100).round(2)
+nanopool_perc = ((nanopool_hr/toHs(network_hr))*100).round(2)
 minergate_perc = ((minergate_hr/network_hr)*100).round(2)
 
 puts ""
@@ -66,7 +68,7 @@ puts "MineXMR:".blue + "	#{minexmr_hr.round(2)} MH/s" + "	#{minexmr_perc}%".bold
 puts "Cryptopool:".blue + "	#{cryptopool_fr_hr.round(2)} MH/s" + "	#{cryptopool_fr_perc}%".bold + " of the network"
 puts "Miningpool Hub:".blue + "	#{miningpoolhub_hr.round(2)} MH/s" + "	#{miningpoolhub_perc}%".bold + " of the network"
 puts "Nanopool:".blue + "	#{nanopool_hr.round(2)} MH/s" + "	#{nanopool_perc}%".bold + " of the network"
-puts "Minergate:".blue + "	#{minergate_hr.round(2)/ 1000000} MH/s" + "	#{minergate_perc}%".bold + " of the network"
+puts "Minergate:".blue + "	#{toHs(minergate_hr).round(2)} MH/s" + "	#{minergate_perc}%".bold + " of the network"
 
 puts ""
 	if (dwarfpool_hr*1000000 || cryptopool_fr_hr*1000000 || minexmr_hr*1000000 || miningpoolhub_hr*1000000 || nanopool_hr*1000000 || minergate_hr)  >= attck_hr
