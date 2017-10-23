f01b3a61ab79f11c9ac3cb74e103a0ca05921056
erciccione <erciccione@users.noreply.github.com>
2017-10-23 19:45:33 +0200

    refactoring

--- 50plus1.rb
+++ 50plus1.rb
@@ -16,64 +16,72 @@ end
 input = "refresh"
 while input == "refresh"
 
-	# get API of all pools
+		# get APIs
+	addrs = {
+	all_pools: 'http://minexmr.com/pools_hist.json',
+	minergate: 'http://moneropools.com/getstats.php?site=minergate.com',
+	network: 'https://moneroblocks.info/api/get_stats'
+	}
 
-string = open('http://minexmr.com/pools_hist.json')
-body = string.read
-pools = JSON.parse body
+	def parse(apis)
+	string = open(apis)
+	@body = string.read
+	end
 
-	# get minergate's API
-string = open('http://moneropools.com/getstats.php?site=minergate.com')
-body = string.read
-minergate_api = JSON.parse body
+	parse(addrs[:all_pools])
+	pools = JSON.parse @body
 
-	# simplify results
-dwarfpool_hr = pools['dwarf'].last
-cryptopool_fr_hr = pools['monero.crypto-pool.fr'].last
-minexmr_hr = pools['minexmr.com'].last
-miningpoolhub_hr = pools['miningpoolhub'].last
-nanopool_hr = pools['nanopool'].last
-minergate_hr = minergate_api["pool"]["hashrate"] 
+	parse(addrs[:minergate])
+	minergate_api = JSON.parse @body
 
-	# get network's Hashrate
-string = open('https://moneroblocks.info/api/get_stats')
-read_string = string.read
-network_hr_raw = JSON.parse read_string
-network_hr = network_hr_raw['hashrate']
+	parse(addrs[:network])
+	network_hr_raw = JSON.parse @body
+
+
+		# simplify results
+	dwarfpool_hr = pools['dwarf'].last
+	cryptopool_fr_hr = pools['monero.crypto-pool.fr'].last
+	minexmr_hr = pools['minexmr.com'].last
+	miningpoolhub_hr = pools['miningpoolhub'].last
+	nanopool_hr = pools['nanopool'].last
+	minergate_hr = minergate_api["pool"]["hashrate"]
+	network_hr = network_hr_raw['hashrate']
+
+
 	
 	
-attck_hr = ((network_hr / 2) * 1.01 )
-fifty_prcnt = (network_hr / 2)
-pc_hr = 40		# in hs/s
-botnum = (attck_hr / pc_hr)
+	attck_hr = ((network_hr / 2) * 1.01 )
+	fifty_prcnt = (network_hr / 2)
+	pc_hr = 40		# in hs/s
+	botnum = (attck_hr / pc_hr)
 
 
-puts "A simple tool which calculates the possibility of a 50+1% attack to the Monero network".bold
-puts ""
-puts "Current global Hashrate:" + " #{toHs(network_hr).round(2)} MH/s".bold
-puts "An attacker should have a hashrate of at least:" + " #{toHs(attck_hr).round(2)} MH/s".bold
-puts "or a botnet with" + " #{botnum.to_i} bots.".bold + " (calculated assuming 1 bot = 40 Hs/s)"
+	puts "A simple tool which calculates the possibility of a 50+1% attack to the Monero network".bold
+	puts ""
+	puts "Current global Hashrate:" + " #{toHs(network_hr).round(2)} MH/s".bold
+	puts "An attacker should have a hashrate of at least:" + " #{toHs(attck_hr).round(2)} MH/s".bold
+	puts "or a botnet with" + " #{botnum.to_i} bots.".bold + " (calculated assuming 1 bot = 40 Hs/s)"
 
 
-	#calculate percentage network
-dwarfpool_perc = ((dwarfpool_hr/toHs(network_hr))*100).round(2)
-cryptopool_fr_perc = ((cryptopool_fr_hr/toHs(network_hr))*100).round(2)
-minexmr_perc = ((minexmr_hr/toHs(network_hr))*100).round(2)
-miningpoolhub_perc = ((miningpoolhub_hr/toHs(network_hr))*100).round(2)
-nanopool_perc = ((nanopool_hr/toHs(network_hr))*100).round(2)
-minergate_perc = ((minergate_hr/network_hr)*100).round(2)
+		#calculate percentage
+	dwarfpool_perc = ((dwarfpool_hr/toHs(network_hr))*100).round(2)
+	cryptopool_fr_perc = ((cryptopool_fr_hr/toHs(network_hr))*100).round(2)
+	minexmr_perc = ((minexmr_hr/toHs(network_hr))*100).round(2)
+	miningpoolhub_perc = ((miningpoolhub_hr/toHs(network_hr))*100).round(2)
+	nanopool_perc = ((nanopool_hr/toHs(network_hr))*100).round(2)
+	minergate_perc = ((minergate_hr/network_hr)*100).round(2)
 
-puts ""
-puts "List of major mining pools and their hashrate:".italic
-puts ""
-puts "Dwarfpool:".blue + "	#{dwarfpool_hr.round(2)} MH/s" + "	#{dwarfpool_perc}%".bold + " of the network"
-puts "MineXMR:".blue + "	#{minexmr_hr.round(2)} MH/s" + "	#{minexmr_perc}%".bold + " of the network"
-puts "Cryptopool:".blue + "	#{cryptopool_fr_hr.round(2)} MH/s" + "	#{cryptopool_fr_perc}%".bold + " of the network"
-puts "Miningpool Hub:".blue + "	#{miningpoolhub_hr.round(2)} MH/s" + "	#{miningpoolhub_perc}%".bold + " of the network"
-puts "Nanopool:".blue + "	#{nanopool_hr.round(2)} MH/s" + "	#{nanopool_perc}%".bold + " of the network"
-puts "Minergate:".blue + "	#{toHs(minergate_hr).round(2)} MH/s" + "	#{minergate_perc}%".bold + " of the network"
+	puts ""
+	puts "List of major mining pools and their hashrate:".italic
+	puts ""
+	puts "Dwarfpool:".blue + "	#{dwarfpool_hr.round(2)} MH/s" + "	#{dwarfpool_perc}%".bold + " of the network"
+	puts "MineXMR:".blue + "	#{minexmr_hr.round(2)} MH/s" + "	#{minexmr_perc}%".bold + " of the network"
+	puts "Cryptopool:".blue + "	#{cryptopool_fr_hr.round(2)} MH/s" + "	#{cryptopool_fr_perc}%".bold + " of the network"
+	puts "Miningpool Hub:".blue + "	#{miningpoolhub_hr.round(2)} MH/s" + "	#{miningpoolhub_perc}%".bold + " of the network"
+	puts "Nanopool:".blue + "	#{nanopool_hr.round(2)} MH/s" + "	#{nanopool_perc}%".bold + " of the network"
+	puts "Minergate:".blue + "	#{toHs(minergate_hr).round(2)} MH/s" + "	#{minergate_perc}%".bold + " of the network"
 
-puts ""
+	puts ""
 	if (dwarfpool_hr*1000000 || cryptopool_fr_hr*1000000 || minexmr_hr*1000000 || miningpoolhub_hr*1000000 || nanopool_hr*1000000 || minergate_hr)  > fifty_prcnt
 	puts "	DANGER: One of the mining pools has reached >50% of the network hashrate !!".red.bold
 	else puts "	None of these pools are close to >50% of the global hashrate".green
@@ -85,6 +93,7 @@ puts ""
 	puts "Type ".italic + "refresh".bold + " to refresh data (new every 60 sec)".italic
 	puts "Type ".italic + "exit".bold + " to close program".italic
 	puts ""
+	
 	input = gets.chomp
 	case input
 	when "exit" then exit! 
